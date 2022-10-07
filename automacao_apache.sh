aebc5a31e9647f872d3fb6c60db9c7f2b13cacaf
Pedro_Camara <pedroocamaraoliveira@gmail.com>
2022-10-07 16:12:31 -0300

    script apache
    Script de automação do apache para bootcamp

---
+++ automacao_apache.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Instalando dependencias..."
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+apt-get install wget -y
+
+echo "Configurando aplicação..."
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio
+cp -R * /var/www/html/
+
+echo "Finalizado!"
